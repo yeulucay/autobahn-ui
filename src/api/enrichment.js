93d02eb2f44c09cf3e5a8ce57d4ce3f7ff6a1286
Yunus Emre Ulucay <ye.ulucay@gmail.com>
2019-07-13 14:14:12 +0300

    ui get enrichment detail data

--- src/api/enrichment.js
+++ src/api/enrichment.js
@@ -1,51 +1,58 @@
-import request from '@/utils/request'
-
+import request from "@/utils/request";
 
 export function getEnrichmentById(enrichmentId) {
   return request({
-    url: '/enrichment/' + enrichmentId,
-    method: 'get'
-  })
+    url: "/enrichment/" + enrichmentId,
+    method: "get"
+  });
 }
 
 export function getEnrichmentList(projectId) {
   return request({
-    url: '/enrichment/list/' + projectId,
-    method: 'get'
-  })
+    url: "/enrichment/list/" + projectId,
+    method: "get"
+  });
 }
 
 export function getEnrichmentSourceList(projectId) {
   return request({
-    url: '/enrichmentsource/list/' + projectId,
-    method: 'get'
-  })
+    url: "/enrichmentsource/list/" + projectId,
+    method: "get"
+  });
 }
 
 export function getFinalStorageList(projectId) {
-    return request({
-      url: '/finalstorage/list/' + projectId,
-      method: 'get'
-    })
-  }
+  return request({
+    url: "/finalstorage/list/" + projectId,
+    method: "get"
+  });
+}
 
 export function getDataTypeList() {
   return request({
-    url: '/commons/datatypes',
-    method: 'get'
-  })
+    url: "/commons/datatypes",
+    method: "get"
+  });
 }
 
 export function getEnrichmentSourceTables(sourceId) {
   return request({
-    url: '/enrichmentsource/table/list/' + sourceId,
-    method: 'get'
-  })
+    url: "/enrichmentsource/table/list/" + sourceId,
+    method: "get"
+  });
 }
 
 export function getEnrichmentSourceColumns(sourceId, columnName) {
   return request({
-    url: '/enrichmentsource/column/list/' + sourceId + "/" + columnName,
-    method: 'get'
-  })
+    url: "/enrichmentsource/column/list/" + sourceId + "/" + columnName,
+    method: "get"
+  });
+}
+
+export function postEnrichment(data) {
+  return request({
+    url: "/enrichment/sync",
+    method: "post",
+    data
+  });
 }
--- src/views/enrichment/detail.vue
+++ src/views/enrichment/detail.vue
@@ -5,172 +5,186 @@
     </el-row>
     <el-row>
       <el-col :span="6">
-        <span class="label">Endpoint:</span>
-        <br>
-        <el-select v-model="enrichment.endPointId" placeholder="Select">
-          <el-option v-for="ep in endpoints" :key="ep.id" :label="ep.name" :value="ep.id"></el-option>
-        </el-select>
-      </el-col>
-      <el-col :span="6">
-        <span class="label">Data Type:</span>
-        <br>
-        <el-select v-model="enrichment.dataTypeId" placeholder="Select">
-          <el-option v-for="dt in dataTypes" :key="dt.id" :label="dt.code" :value="dt.id"></el-option>
-        </el-select>
-      </el-col>
-      <el-col :span="6">
-        <span class="label">Enrichment Source:</span>
-        <br>
-        <el-select
-          v-model="enrichment.enrichmentSourceId"
-          @change="onEnrichmentSourceChange"
-          placeholder="Select"
-        >
-          <el-option v-for="es in enrichmentSources" :key="es.id" :label="es.name" :value="es.id"></el-option>
-        </el-select>
-      </el-col>
-      <el-col :span="6">
-        <span class="label">Final Storage:</span>
-        <br>
-        <el-select v-model="enrichment.finalStorageId" placeholder="Select">
-          <el-option v-for="fs in finalStorages" :key="fs.id" :label="fs.name" :value="fs.id"></el-option>
-        </el-select>
-      </el-col>
-    </el-row>
-    <el-row class="row-margin">
-      <el-button type="primary" @click="addEnrichment" round>
-        Add
-        <i class="el-icon-plus el-icon-right"></i>
-      </el-button>
-      <el-button type="success" @click="saveEnrichment" round style="float:right">Save</el-button>
-      <el-button type="infor" @click="cancelEnrichment" round style="float:right">Cancel</el-button>
-    </el-row>
-
-    <el-tabs style="margin-top:15px;" type="border-card">
-      <el-tab-pane label="Matches">
-        <el-row v-for="(item, i) in enrichmentModel" v-bind:key="i" class="match">
-          <el-row>
-            <el-col :span="1">
-              <i class="el-icon-error close-icon" @click="closeMatch(i)"></i>
-            </el-col>
-            <el-col :span="11">
-              <span class="label">Attribute Match:</span>
-              <el-input
-                v-model="item.selectedAttr"
-                @keyup.enter.native="attrEnter(i)"
-                placeholder="Enter new attribute"
-              />
-              <el-drag-select
-                v-model="item.matchedAttrList"
-                style="width:100%;"
-                multiple
-                placeholder="select"
-              >
-                <el-option
-                  v-for="col in item.sourceAttrNames"
-                  :label="col"
-                  :value="col"
-                  :key="col"
-                />
-              </el-drag-select>
-            </el-col>
-            <el-col :span="11" :offset="1">
-              <span class="label">Column Match:</span>
-              <br>
-              <el-select
-                v-model="item.selectedTable"
-                style="width: 100%"
-                @change="onTableChange(i)"
-                placeholder="Select"
-              >
-                <el-option v-for="t in tableNames" :key="t" :label="t" :value="t"></el-option>
-              </el-select>
-              <br>
-              <el-drag-select
-                v-model="item.matchedColumnList"
-                style="width:100%;"
-                multiple
-                placeholder="select"
-              >
-                <el-option v-for="col in item.columnNames" :label="col" :value="col" :key="col"/>
-              </el-drag-select>
-            </el-col>
-          </el-row>
-          <el-row class="row-margin" v-for="(join,j) in item.joinModel" v-bind:key="j">
-            <!-- join -->
-
-            <el-col :span="11" :offset="1">
-              <span class="label">{{j+1}} Join Source:</span>
-              <br>
-              <el-select
-                v-model="join.joinSourceTable"
-                style="width: 100%"
-                @change="onJoinTableChange(i, j, true)"
-                placeholder="Select"
-              >
-                <el-option v-for="t in tableNames" :key="t" :label="t" :value="t"></el-option>
-              </el-select>
-              <br>
-              <el-drag-select
-                v-model="join.joinSourceMatchList"
-                style="width:100%;"
-                multiple
-                placeholder="select"
-              >
-                <el-option
-                  v-for="col in join.joinSourceColumns"
-                  :label="col"
-                  :value="col"
-                  :key="col"
-                />
-              </el-drag-select>
-            </el-col>
-            <el-col :span="11" :offset="1">
-              <span class="label">{{j+1}} Join Destination:</span>
-              <br>
-              <el-select
-                v-model="join.joinDestTable"
-                style="width: 100%"
-                @change="onJoinTableChange(i, j, false)"
-                placeholder="Select"
-              >
-                <el-option v-for="t in tableNames" :key="t" :label="t" :value="t"></el-option>
-              </el-select>
-              <br>
-              <el-drag-select
-                v-model="join.joinDestMatchList"
-                style="width:100%;"
-                multiple
-                placeholder="select"
-              >
-                <el-option
-                  v-for="col in join.joinDestColumns"
-                  :label="col"
-                  :value="col"
-                  :key="col"
-                />
-              </el-drag-select>
-            </el-col>
-          </el-row>
-          <el-row>
-            <el-button type="text" @click="addJoinModel(i)">join table</el-button>
-          </el-row>
+        <el-row class="row-margin">
+          <span class="label">Name:</span>
+          <br />
+          <el-input v-model="enrichment.name" />
         </el-row>
-      </el-tab-pane>
-      <el-tab-pane label="Enrichment Data">
-        <span class="label">Select Final Storage Columns:</span>
-        <br>
-        <el-row class="row-margin" v-for="(list, i) in totalList" v-bind:key="i">
-          <el-checkbox @change="deneme" v-model="list.checked" :label="list.value"></el-checkbox>
+        <el-row class="row-margin">
+          <span class="label">Endpoint:</span>
+          <br />
+          <el-select class="select-style" v-model="enrichment.endPointId" placeholder="Select">
+            <el-option v-for="ep in endpoints" :key="ep.id" :label="ep.name" :value="ep.id"></el-option>
+          </el-select>
+        </el-row>
+        <el-row class="row-margin">
+          <span class="label">Data Type:</span>
+          <br />
+          <el-select class="select-style" v-model="enrichment.dataTypeId" placeholder="Select">
+            <el-option v-for="dt in dataTypes" :key="dt.id" :label="dt.code" :value="dt.id"></el-option>
+          </el-select>
+        </el-row>
+        <el-row class="row-margin">
+          <span class="label">Enrichment Source:</span>
+          <br />
+          <el-select
+            class="select-style"
+            v-model="enrichment.enrichmentSourceId"
+            @change="onEnrichmentSourceChange"
+            placeholder="Select"
+          >
+            <el-option v-for="es in enrichmentSources" :key="es.id" :label="es.name" :value="es.id"></el-option>
+          </el-select>
+        </el-row>
+        <el-row class="row-margin">
+          <span class="label">Final Storage:</span>
+          <br />
+          <el-select class="select-style" v-model="enrichment.finalStorageId" placeholder="Select">
+            <el-option v-for="fs in finalStorages" :key="fs.id" :label="fs.name" :value="fs.id"></el-option>
+          </el-select>
+        </el-row>
+      </el-col>
+      <el-col :span="18" style="padding-left:16px">
+        <el-row class="row-margin">
+          <el-button type="primary" @click="addEnrichment" round>
+            Add
+            <i class="el-icon-plus el-icon-right"></i>
+          </el-button>
+          <el-button type="success" @click="saveEnrichment" round style="float:right">Save</el-button>
+          <el-button type="infor" @click="cancelEnrichment" round style="float:right">Cancel</el-button>
+        </el-row>
+        <el-tabs style="margin-top:15px;" type="border-card">
+          <el-tab-pane label="Matches">
+            <el-row v-for="(item, i) in enrichmentModel" v-bind:key="i" class="match">
+              <el-row>
+                <el-col :span="1">
+                  <i class="el-icon-error close-icon" @click="closeMatch(i)"></i>
+                </el-col>
+                <el-col :span="11">
+                  <span class="label">Attribute Match:</span>
+                  <el-input
+                    v-model="item.selectedAttr"
+                    @keyup.enter.native="attrEnter(i)"
+                    placeholder="Enter new attribute"
+                  />
+                  <el-drag-select
+                    v-model="item.matchedAttrList"
+                    style="width:100%;"
+                    multiple
+                    placeholder="select"
+                  >
+                    <el-option
+                      v-for="col in item.sourceAttrNames"
+                      :label="col"
+                      :value="col"
+                      :key="col"
+                    />
+                  </el-drag-select>
+                </el-col>
+                <el-col :span="11" :offset="1">
+                  <span class="label">Column Match:</span>
+                  <br />
+                  <el-select
+                    v-model="item.selectedTable"
+                    style="width: 100%"
+                    @change="onTableChange(i)"
+                    placeholder="Select"
+                  >
+                    <el-option v-for="t in tableNames" :key="t" :label="t" :value="t"></el-option>
+                  </el-select>
+                  <br />
+                  <el-drag-select
+                    v-model="item.matchedColumnList"
+                    style="width:100%;"
+                    multiple
+                    placeholder="select"
+                  >
+                    <el-option
+                      v-for="col in item.columnNames"
+                      :label="col"
+                      :value="col"
+                      :key="col"
+                    />
+                  </el-drag-select>
+                </el-col>
+              </el-row>
+              <el-row class="row-margin" v-for="(join,j) in item.joinModel" v-bind:key="j">
+                <!-- join -->
 
-          <!-- 
+                <el-col :span="11" :offset="1">
+                  <span class="label">{{j+1}} Join Source:</span>
+                  <br />
+                  <el-select
+                    v-model="join.joinSourceTable"
+                    style="width: 100%"
+                    @change="onJoinTableChange(i, j, true)"
+                    placeholder="Select"
+                  >
+                    <el-option v-for="t in tableNames" :key="t" :label="t" :value="t"></el-option>
+                  </el-select>
+                  <br />
+                  <el-drag-select
+                    v-model="join.joinSourceMatchList"
+                    style="width:100%;"
+                    multiple
+                    placeholder="select"
+                  >
+                    <el-option
+                      v-for="col in join.joinSourceColumns"
+                      :label="col"
+                      :value="col"
+                      :key="col"
+                    />
+                  </el-drag-select>
+                </el-col>
+                <el-col :span="11" :offset="1">
+                  <span class="label">{{j+1}} Join Destination:</span>
+                  <br />
+                  <el-select
+                    v-model="join.joinDestTable"
+                    style="width: 100%"
+                    @change="onJoinTableChange(i, j, false)"
+                    placeholder="Select"
+                  >
+                    <el-option v-for="t in tableNames" :key="t" :label="t" :value="t"></el-option>
+                  </el-select>
+                  <br />
+                  <el-drag-select
+                    v-model="join.joinDestMatchList"
+                    style="width:100%;"
+                    multiple
+                    placeholder="select"
+                  >
+                    <el-option
+                      v-for="col in join.joinDestColumns"
+                      :label="col"
+                      :value="col"
+                      :key="col"
+                    />
+                  </el-drag-select>
+                </el-col>
+              </el-row>
+              <el-row>
+                <el-button type="text" @click="addJoinModel(i)">join table</el-button>
+              </el-row>
+            </el-row>
+          </el-tab-pane>
+          <el-tab-pane label="Enrichment Data">
+            <span class="label">Select Final Storage Columns:</span>
+            <br />
+            <el-row class="row-margin" v-for="(list, i) in totalList" v-bind:key="i">
+              <el-checkbox @change="deneme" v-model="list.checked" :label="list.value"></el-checkbox>
+
+              <!-- 
           <el-row class="row-margin span-item" v-for="item in list.all" v-bind:key="item">
             <el-checkbox @change="onEnrichmentDataSelected" label="item"></el-checkbox>
-          </el-row>-->
-        </el-row>
-      </el-tab-pane>
-      <el-tab-pane label></el-tab-pane>
-    </el-tabs>
+              </el-row>-->
+            </el-row>
+          </el-tab-pane>
+          <el-tab-pane label></el-tab-pane>
+        </el-tabs>
+      </el-col>
+    </el-row>
   </div>
 </template>
 <script>
@@ -182,7 +196,8 @@ import {
   getFinalStorageList,
   getDataTypeList,
   getEnrichmentSourceTables,
-  getEnrichmentSourceColumns
+  getEnrichmentSourceColumns,
+  postEnrichment
 } from "@/api/enrichment";
 import { getEndpointTypes, getEndpointList } from "@/api/endpoint";
 import { getProjectList } from "@/api/project";
@@ -238,9 +253,20 @@ export default {
       data["matchModel"] = this.enrichmentModel;
       data["selectedColumnList"] = this.totalList;
 
-      console.log(data);
-
-      //TODO: data'yi db'ye kaydet (stringify)
+      this.enrichment.enrichmentRules = JSON.stringify(data);
+      // console.log(this.enrichment);
+      postEnrichment(this.enrichment);
+    },
+    organizeRules() {
+      var detail = JSON.parse(this.enrichment.enrichmentRules);
+      if (detail) {
+        this.enrichmentModel = detail.matchModel;
+        this.totalList = detail.selectedColumnList;
+      }
+    },
+    clearRules() {
+      this.enrichmentModel = [];
+      this.totalList = [];
     },
     cancelEnrichment() {},
     addJoinModel(i) {
@@ -266,10 +292,6 @@ export default {
         matchedColumnList: [],
         joinModel: []
       });
-      // this.totalList.push({
-      //   all:[],
-      //   selected: []
-      // });
     },
     attrEnter(i) {
       this.enrichmentModel[i].sourceAttrNames.push(
@@ -295,9 +317,11 @@ export default {
           this.getEnrichmentSources(this.enrichment.projectId);
           this.getFinalStorages(this.enrichment.projectId);
           this.getTableNames(this.enrichment.enrichmentSourceId);
+          this.organizeRules();
         }
       });
     },
+
     getEnrichmentSources(projectId) {
       getEnrichmentSourceList(projectId).then(response => {
         if (response.data) {
@@ -351,6 +375,7 @@ export default {
       });
     },
     onEnrichmentSourceChange(value) {
+      this.clearRules();
       this.getTableNames(value);
     },
     onTableChange(i) {
@@ -429,6 +454,10 @@ export default {
   color: #7a7a7a;
   font-size: medium;
   padding-bottom: 10px;
+}
+
+.select-style {
+  width: 100%;
 }
 
 .board-column {
